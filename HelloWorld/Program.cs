7f5bc1c084fac597134587d67060c52cdc0f5892
Shawn <deshawn.dyson.career@gmail.com>
2020-03-30 19:58:44 -0400

    This is the initial commit. Simply the first commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.Clear();
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello DeShawn");
+            Console.Read(); // This allows the console to stay open after running the program and executing the functions
+        }
+    }
+}
